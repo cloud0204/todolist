d50105d661cb132344db2624a1826a9f9438b2a9
cloud <cloud7777222@yahoo.com.tw>
2020-11-07 22:44:22 +0800

    update

--- vue.confing.js
+++ vue.confing.js
@@ -1,7 +1,8 @@
 //vue.confing.js
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/production-sub-path/'
-    : '/'
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/production-sub-path/'
+  //   : '/'
+  publicPath:'./'
 }
