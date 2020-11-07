6dea79ad6730803e1da0a594cae730bfdeb6df21
cloud <cloud7777222@yahoo.com.tw>
2020-11-08 00:58:29 +0800

    update

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,5 @@
-// module.exports = {
-//   presets: [
-//     '@vue/cli-plugin-babel/preset'
-//   ]
-// }
 module.exports = {
   presets: [
-    '@vue/app',
-  ],
-};
+    '@vue/cli-plugin-babel/preset'
+  ]
+}
