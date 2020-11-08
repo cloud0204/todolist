04fad12d639f7721efcf94171a3f91e07b8e26a0
cloud <cloud7777222@yahoo.com.tw>
2020-11-08 14:57:15 +0800

    regrgr

--- vue.confing.js
+++ vue.confing.js
@@ -10,7 +10,6 @@ module.exports = {
   //  publicPath: process.env.NODE_ENV === 'production'
   //   ? '/todolist/'
   //   : ''
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/todolist/'
-    : './' 
+    publicPath: '/todolist/'
+
 }
